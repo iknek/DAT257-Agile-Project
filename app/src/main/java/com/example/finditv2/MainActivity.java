41c78ebbf00fc08fcd3d473849c374bb778fe192
karagenzie <g.geraghty@hotmail.com>
2022-04-05 19:08:52 +0200

    -Added back buttons -Created screen for displaying items (not finished)

--- app/src/main/java/com/example/finditv2/MainActivity.java
+++ app/src/main/java/com/example/finditv2/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.finditv2;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.RecyclerView;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +20,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TextView showItems = (TextView) findViewById(R.id.item_list);
+        //showItems.setText("hej");
     }
 
     public void printHi(View view) {
         setContentView(R.layout.new_item_screen);
+    }
+
+    public void backToMain(View view) {
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToItemScreen(View view) {
+        setContentView(R.layout.item_screen);
+        //showItems.setText("hej");
     }
 
     public void saveItem(View view){

9c5c7191150f22afef4de68b675b040d0708b60e
johror <73944123+johror@users.noreply.github.com>
2022-04-08 11:14:11 +0200

    Added documentation for constructor, Item

--- app/src/main/java/com/example/finditv2/Item.java
+++ app/src/main/java/com/example/finditv2/Item.java
@@ -5,7 +5,10 @@ import java.io.Serializable;
 public class Item implements Serializable {
 
     String description;
-
+    /**
+     * Item constructor.
+     * @param string = Item description
+     */
     public Item(String string){
         this.description = string;
 

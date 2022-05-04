4fca79e6bc68ab06fbdf1a45e8d21d9fdbc50711
andol <andolo7.5@gmail.com>
2022-05-04 16:01:24 +0200

    Use camera or gallery to get a picture of the lost item

--- app/src/main/java/com/example/finditv2/AddItemScreen.java
+++ app/src/main/java/com/example/finditv2/AddItemScreen.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,6 +14,10 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.*;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
@@ -35,6 +40,8 @@ public class AddItemScreen extends AppCompatActivity {
     private static final int CAMERA_PIC_REQUEST = 1337;
     private String imagePath;
     private Bitmap bitmap;
+
+    private int requestCode;
 
     //Använder för att identifiera bilderna
     private Date date;
@@ -151,23 +158,30 @@ public class AddItemScreen extends AppCompatActivity {
     private void openGallery() {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-
-        Intent chooser = Intent.createChooser(galleryIntent, "Some text here");
+        Intent chooser = Intent.createChooser(galleryIntent, "Camera or gallery");
         chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] { cameraIntent });
-        startActivityForResult(chooser, PICK_IMAGE);
+        someActivityResultLauncher.launch(chooser);
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+        new ActivityResultContracts.StartActivityForResult(),
+        new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            Intent data = result.getData();
+
             Uri imageUri = data.getData();
             imageView.setImageURI(imageUri);
-            bitmap = null;
-            try {
-                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (data.hasExtra("data")) {
+                bitmap = (Bitmap) data.getExtras().get("data");
+                imageView.setImageBitmap(bitmap);
+            } else {
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
-    }
+    });
 }
